952c8306eee94d58379c003115e017278d09611f
Stefan Csomor <csomor@advancedconcepts.ch>
2014-07-05 10:14:32 +0000

    remove carbon build temporarily
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@76830 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/generic/listctrl.cpp
+++ src/generic/listctrl.cpp
@@ -1420,6 +1420,13 @@ wxListTextCtrlWrapper::wxListTextCtrlWrapper(wxListMainWindow *owner,
 
 void wxListTextCtrlWrapper::EndEdit(EndReason reason)
 {
+    if( m_aboutToFinish )
+    {
+        // We already called Finish which cannot be called
+        // more than once.
+        return;
+    }
+
     m_aboutToFinish = true;
 
     switch ( reason )
@@ -1525,6 +1532,7 @@ void wxListTextCtrlWrapper::OnKillFocus( wxFocusEvent &event )
 {
     if ( !m_aboutToFinish )
     {
+        m_aboutToFinish = true;
         if ( !AcceptChanges() )
             m_owner->OnRenameCancelled( m_itemEdited );
 
@@ -2418,6 +2426,9 @@ void wxListMainWindow::OnMouse( wxMouseEvent &event )
         {
             // reset the selection and bail out
             HighlightAll(false);
+
+            if ( event.LeftUp() )
+                event.Skip();
         }
 
         return;
@@ -2493,6 +2504,8 @@ void wxListMainWindow::OnMouse( wxMouseEvent &event )
         }
 
         m_lineSelectSingleOnUp = (size_t)-1;
+
+        event.Skip();
     }
     else
     {
