29660db0bfc4a78336821305596bf9cb45cb0c8a
Pavel Kalugin <p.kalugin@gmail.com>
2018-09-07 13:44:41 +0300

    Add wxGrid functions to set cell data type
    This adds functions like wxGrid::SetCellFormatBool() which are similar
    to wxGrid::SetColFormatBool() but for individual cells.

--- include/wx/generic/grid.h
+++ include/wx/generic/grid.h
@@ -1358,6 +1358,13 @@ public:
     void     SetColFormatDate(int col);
     void     SetColFormatCustom(int col, const wxString& typeName);
 
+    // same for individual cells
+    void     SetCellFormatBool(int row, int col);
+    void     SetCellFormatNumber(int row, int col);
+    void     SetCellFormatFloat(int row, int col, int width = -1, int precision = -1);
+    void     SetCellFormatDate(int row, int col);
+    void     SetCellFormatCustom(int row, int col, const wxString& typeName);
+
     // ------ row and col formatting
     //
     int      GetDefaultRowSize() const;
